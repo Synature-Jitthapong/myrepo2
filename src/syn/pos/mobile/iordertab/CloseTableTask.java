3ac98553cf2c663e243ab49af1d162fa5c13f197
Synature-Jitthapong <jitthapong@synaturegroup.com>
2013-07-12 16:08:41 +0700

    modify show dalla if table is pay

---
+++ src/syn/pos/mobile/iordertab/CloseTableTask.java
@@ -0,0 +1,22 @@
+package syn.pos.mobile.iordertab;
+
+import android.content.Context;
+
+public class CloseTableTask extends WebServiceTask{
+
+	public CloseTableTask(Context c, GlobalVar gb) {
+		super(c, gb, "");
+	}
+
+	@Override
+	protected void onPreExecute() {
+		
+	}
+
+	@Override
+	protected void onPostExecute(String result) {
+		
+	}
+
+	
+}
