6f803cff377243555dd9296fe1028fb2dec23fa9
Synature-Jitthapong <jitthapong@synaturegroup.com>
2013-08-09 17:50:06 +0700

    update

--- gen/syn/pos/mobileposlibs/R.java
+++ gen/syn/pos/mobileposlibs/R.java
@@ -8,7 +8,7 @@ package syn.pos.mobileposlibs;
 
 public final class R {
 	public static final class drawable {
-		public static final int ic_launcher = 0x7f020039;
+		public static final int ic_launcher = 0x7f02003a;
 	}
 	public static final class string {
 		public static final int app_name = 0x7f0b0000;
