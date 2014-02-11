b97d8289e33d73c63aeeb2f7bf15bfeb6a10d230
Synature-Jitthapong <jitthapong@synaturegroup.com>
2014-02-11 13:40:03 +0700

    payment info

---
+++ src/com/syn/iorder/PayinfoBuilder.java
@@ -0,0 +1,12 @@
+package com.syn.iorder;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+public class PayinfoBuilder extends AlertDialog.Builder{
+
+	public PayinfoBuilder(Context context) {
+		super(context);
+	}
+
+}
