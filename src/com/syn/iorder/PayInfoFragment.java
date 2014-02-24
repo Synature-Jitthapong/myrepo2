0e23a9439099300c5200875f5504bd74ae842d19
Synature-Jitthapong <jitthapong@synaturegroup.com>
2014-02-24 17:06:17 +0700

    merge issue#14, #16

--- src/com/syn/iorder/PayInfoFragment.java
+++ src/com/syn/iorder/PayInfoFragment.java
@@ -15,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
@@ -74,6 +75,9 @@ public class PayInfoFragment extends DialogFragment{
 					mTotalPay = totalPay;
 					setPay();
 					setChange();
+					InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(
+						      Context.INPUT_METHOD_SERVICE);
+						imm.hideSoftInputFromWindow(mTxtTotalPrice.getWindowToken(), 0);
 					return true;
 				}
 				return false;
